d2099f7d01437b5e1ea8e19612a3e969193ac179
Oleg Shalyukov <heilagr.ercera@gmail.com>
2023-05-27 23:24:51 +0700

    Add new tasks

---
+++ 6 kyu/Skyscrapers Area.js
@@ -0,0 +1,77 @@
+/*
+
+We want to find the area of a set of buildings stacked close to each other.
+
+A matrix (an array of arrays) gives us a description of the building from above, where each number represents the height expressed in cubes 1*1*1, the cubes are stacked adjacent to each other.
+
+For example:
+
+matrix = [[2,1],[2,0]] 
+
+In position (0,0), there are 2 cubes stacked on each other
+In position (0,1), there is only 1 cube
+In position (1,0), there are 2 cubes stacked on each other
+In position (1,1), there are 0 cubes stacked on each other.
+
+So, the total covered area is 20.
+The matrix will be of at least size 1*1 and square.
+
+Note: The floor of the building counts as area as well.
+
+*/
+
+/*
+
+Мы хотим найти площадь группы зданий, расположенных близко друг к другу.
+
+Матрица (массив массивов) дает нам описание здания сверху, где каждое число представляет высоту в кубах 1*1*1, кубы расположены рядом друг с другом.
+
+Например:
+
+matrix = [[2,1],[2,0]]
+
+В позиции (0,0) находятся 2 куба, расположенные друг на друге
+В позиции (0,1) есть только 1 куб
+В позиции (1,0) находятся 2 куба, расположенные друг на друге
+В позиции (1,1) нет кубов, расположенных друг на друге.
+
+Таким образом, общая площадь составляет 20.
+Матрица будет иметь размер как минимум 1*1 и будет квадратной.
+
+Примечание: пол здания также учитывается в площади.
+
+*/
+
+function surface_area(matrix) {
+    let area = 0; // Переменная, в которую вернем конечное значение
+    for (let i = 0; i < matrix.length; i++) { // Проверка значениц первого массива из двух [[i][j]]
+        for (let j = 0; j < matrix.length; j++) { // Проверка значениц второго массива из двух [[i][j]]
+            const height = matrix[i][j]; // Создаем переменную, в которую запишем двумерный массив
+            const arrLeng = matrix.length; // Создаем переменную, в которую записываем длинну вводного массива
+            if (height > 0) { // Условие при котором двумерный массив не должен быть равен нулю
+                area += 2 // В теле цикла добавляем к каждому значению area + 2
+                    +
+                    (i !== 0 // Проверка первого условия
+                        ? Math.max(0, height - matrix[i - 1][j])
+                        : height)
+                    +
+                    (i !== arrLeng - 1 // Проверка второго условия
+                        ? Math.max(0, height - matrix[i + 1][j])
+                        : height)
+                    +
+                    (j !== 0 // Проверка третьего условия
+                        ? Math.max(0, height - matrix[i][j - 1])
+                        : height)
+                    +
+                    (j !== arrLeng - 1 // Проверка четвертого условия
+                        ? Math.max(0, height - matrix[i][j + 1])
+                        : height);
+            }
+        }
+    }
+    return area;
+}
+
+// Не совсем красивая форма записи, перепишу её через switch чуть позже добавлю обновление
+
+console.log(surface_area([[2, 1], [2, 0]]));
---
+++ 6 kyu/Sum of a Sequence [Hard-Core Version].js
@@ -0,0 +1,49 @@
+/*
+
+As the title suggests, this is the hard-core version of another neat kata.
+
+The task is simple to explain: simply sum all the numbers from the first parameter being the beginning to the second parameter being the upper limit (possibly included), going in steps expressed by the third parameter:
+
+sequenceSum(2,2,2) === 2
+sequenceSum(2,6,2) === 12 // 2 + 4 + 6
+sequenceSum(1,5,1) === 15 // 1 + 2 + 3 + 4 + 5
+sequenceSum(1,5,3) === 5 // 1 + 4
+If it is an impossible sequence (with the beginning being larger the end and a positive step or the other way around), just return 0. See the provided test cases for further examples :)
+
+Note: differing from the other base kata, much larger ranges are going to be tested, so you should hope to get your algo optimized and to avoid brute-forcing your way through the solution.
+
+*/
+
+/*
+
+Хардкор версия еще одной ката.
+
+Задача состоит в том, что: 
+нужно просуммировать все числа от первого параметра, являющегося началом, 
+до второго параметра, являющегося верхним пределом (возможно, включенным), 
+шагом указанным в третьем параметре
+
+Если невозможная последовательность: 
+- начало больше конца (begin > end)
+- положительным шагом или наоборот (step != end - begin)
+Вернуть 0. 
+
+*/
+
+function sequenceSum(begin, end, step) {
+    //your code here
+    let result = 0; // Объявляем новую переменную, в которую будет выведен конечный результат
+    for ( // Решение, при помощи цикла, как и в простом варианте решения данной задачи
+        let i = begin; // Присваиваем переменной "i" значение begin(первого операнда)
+        // используем тернарный оператор для наглядности и меньшего количества символов
+
+        step > 0 // Если шаг будет больше нуля, то
+            ? i <= end // Переменная "i" меньше второго операнда
+            : i >= end; // Переменная "i" больше или равна второму операнду
+
+        i += step) // Присваеваем циклу шаг значение третьего операнда "step"
+        result += i; // Каждую итерацию к результату добавляем значение "i"
+    return result; // Возвращаем переменную после цикла. Которая будет содержать сумму всех значений "i". 
+}
+
+console.log(sequenceSum(1, 5, 0));
